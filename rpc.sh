b80b163155dd6a6b70c308ffdc7d000dec664d8a
PedroLimposteur <82096611+PedroLimposteur@users.noreply.github.com>
2021-04-07 15:27:41 +0200

    Computer can now play normally

--- rpc.sh
+++ rpc.sh
@@ -3,18 +3,28 @@ read player_choice
 player_choice="${player_choice,,}" # to lower case
 
 choices=(rock paper scissors)
-for choice in "${choices[@]}"; do
-	if [ "$player_choice" == $choice ]; then ok=1; fi
+for i in "${!choices[@]}"; do
+	if [ "$player_choice" == "${choices[$i]}" ]; then player_index=$i; fi
 done
 
-if [ -z $ok ]; then
+if [ -z $player_index ]; then
 	echo "You must choose rock paper or scissors"
 	exit
 fi
 
 
 if [ $1 ] && [ "$1" == "--no-cheat" ]; then
-	echo "y u want no cheat"
+	index=$(($RANDOM % 3))
+	ai_choice=${choices[$index]}
+	for i in "${!choices[@]}"; do
+		if [ "$ai_choice" == "${choices[$i]}" ]; then ai_index=$i; fi
+	done
+
+	echo "I do $ai_choice"
+	if [ $player_choice == $ai_choice ]; then echo "Is tie. Fun game."
+	elif [ $(( ( $player_index + 1 ) % 3 )) -eq $ai_index ]; then echo "You weak, I strong, you lose."
+	else echo "Hey, you said no cheat"
+	fi
 else
 	if [ $player_choice == "rock" ]; then echo "I cast bigger rock. I win"
 	elif [ $player_choice == "paper" ]; then echo "I cast fire. I win"
